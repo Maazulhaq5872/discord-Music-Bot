b880c4eb0280065285273f4806efa32cb1107fba
Maaz-ul-Haque <maazulhaq5872@gmail.com>
2021-03-22 00:09:54 +0500

    Music

---
+++ commands/leave.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'leave',
+    description: 'To stop the bot from playing music and leave the channel',
+    execute(message, args) {
+        const voicechannel = message.member.voice.channel;
+        if(!voicechannel) return message.channel.send("Voice channel join kar bsdk uske leave likhna");
+        await voicechannel.leave();
+        await message.channel.send('Acha chalta hun, duaon main yaad rakhna :smiling_face_with_tear:')
+    }
+}
