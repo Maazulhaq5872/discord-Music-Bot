9142226a525324c86e6bb3f5841a32abf5f9883f
Maaz-ul-Haque <maazulhaq5872@gmail.com>
2021-03-24 02:54:40 +0500

    Starting over

--- README.md
+++
@@ -1,1 +0,0 @@
-# disocord-bot
--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: ' ping command',
-    execute(message, args, client){
-        message.channel.send('pong')
-    }
-};
