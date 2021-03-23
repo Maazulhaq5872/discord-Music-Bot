199335965997287b5eaa62f8a0407085ff6abff7
Maaz-ul-Haque <maazulhaq5872@gmail.com>
2021-03-23 13:13:09 +0500

    improvements

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 
-const prefix = '/';
 
 const fs = require('fs');
 
@@ -14,25 +13,16 @@ for(const file of command_files){
     client.commands.set(command.name, command);
 }
 
-client.once('ready', ready=>{
-    console.log("ANALysis Is Online!");
-})
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
-client.on('message', message=>{
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (!client.commands.has(command)) return;
-
-    try {
-        client.commands.get(command).execute(message, args);    
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
-    }
-})
-
-client.login('ODIzMTA5NDI1NjA1OTAyMzQ2.YFcCYw.m0-b9-pN1ONs9iPu2hEBX0BASEc') // kOriginal bot
-// client.login('ODIzNTYxMTY2MjEwMjAzNjk4.YFinGw.OUm3QISXLYXE2GHAgk8At4qzJzA') // Test bot
+client.login('ODIzMTA5NDI1NjA1OTAyMzQ2.YFcCYw.m0-b9-pN1ONs9iPu2hEBX0BASEc') // Original bot
+//client.login('ODIzNTYxMTY2MjEwMjAzNjk4.YFinGw.OUm3QISXLYXE2GHAgk8At4qzJzA') // Test bot
