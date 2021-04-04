2bf40120a762f72ce15820d1952b067da4bd0626
Maaz-ul-Haque <maazulhaq5872@gmail.com>
2021-04-04 15:30:18 +0500

    maybe fixed!

--- Music-Bot_1/commands/music.js
+++ Music-Bot_1/commands/music.js
@@ -24,14 +24,9 @@ module.exports = {
                 const song_INFO = await ytdl.getInfo(args[0]);
                 song = {title: song_INFO.videoDetails.title, url: song_INFO.videoDetails.video_url}
             } else {
-                try {
-                    const music_Finder = async (query) =>{
-                        const music_Result = await ytsearch(query);
-                        return (music_Result.videos.length > 1) ? music_Result.videos[0] : null;
-                    }
-                } catch (error) {
-                    console.error(error);
-                    message.reply('there was an error trying to execute that command!');
+                const music_Finder = async (query) =>{
+                    const music_Result = await ytsearch(query);
+                    return (music_Result.videos.length > 1) ? music_Result.videos[0] : null;
                 }
                 const music = await music_Finder(args.join(' '));
                 if(music){
