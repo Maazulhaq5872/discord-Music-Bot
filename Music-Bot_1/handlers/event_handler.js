54bb5fe825c35ba85ddd6cf29dc2a09d02553d51
Maaz-ul-Haque <maazulhaq5872@gmail.com>
2021-04-04 19:24:41 +0500

    maybe fixed!

--- Music-Bot_1/handlers/event_handler.js
+++ Music-Bot_1/handlers/event_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports =(client, Discord)=>{
     const load_dir = (dirs)=>{
-        const eventFiles = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
+        const eventFiles = fs.readdirSync(`./Music-Bot_1/events/${dirs}`).filter(file => file.endsWith('.js'));
         for (const file of eventFiles) {
 	        const event = require(`../events/${dirs}/${file}`);
 	        if (event.once) {
