eaca54cedad4e66b1504908a5c00ff1a9e869783
Muhammad Maaz <63790715+Maazulhaq5872@users.noreply.github.com>
2022-12-13 23:29:31 +0500

    Removed Music-Bot_1

--- Music-Bot_1/handlers/command_handler.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-module.exports = (client, Discord)=>{
-    const command_files = fs.readdirSync(`./Music-Bot_1/commands/`).filter(file => file.endsWith('.js'));
-    for(const file of command_files){
-        const command = require(`../commands/${file}`);
-        client.commands.set(command.name, command);
-    }
-}
